fc1bef0813a01c998154dda419a5c05a10c4ecf6
Chen Yuxian <470963817@qq.com>
2018-06-09 23:12:40 +0800

    把contact恢复原样

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,22 +1,13 @@
 <template>
   <div id="con">
     <div>this is contact page</div>
-    <button @click="click">catch data</button>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    click : function() {
-      
-    }
-  }
 }
 </script>
 
 <style>
-#con {
-  padding-top: 100px;
-}
 </style>
